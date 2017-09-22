906e969ff496dccf782d530ddab015c2ee264743
Mati <mfraga@itba.edu.ar>
2017-09-22 20:10:58 -0300

    Add game interactions

---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class BallController : MonoBehaviour {
+
+    private Rigidbody rb;
+
+    void Start() {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate() {
+        if (transform.position.z < -13) { 
+            transform.position = new Vector3(-1, 1.5f, 5f);
+            rb.velocity = Vector3.zero;
+        }
+    }
+}
